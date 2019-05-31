70f7fb2e738f5c3108bdc1a9bcb43f91127745ce
Marcin Drzewiecki <marcin9504@wp.pl>
2019-05-31 16:08:45 +0200

    Fixed model images size: reverted downscaling images

--- app/src/main/java/put/poznan/tensorflow/MainActivity.java
+++ app/src/main/java/put/poznan/tensorflow/MainActivity.java
@@ -148,9 +148,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
             Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-//            Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
+            Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
             imageView.setImageBitmap(bitmap);
-            final List<Classifier.Recognition> results = classifier.recognizeImage(bitmap);
+            final List<Classifier.Recognition> results = classifier.recognizeImage(scaledBitmap);
             textView.setText(results.toString());
             buttonShare.setVisibility(View.VISIBLE);
         }
